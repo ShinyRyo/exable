7e54935c3d017d8e098b99f412370ec456a1b29c
ShinyRyo <tan0ry0shiny@gmail.com>
2022-07-25 14:43:05 +0900

    first commit

---
+++ exable/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExableConfig(AppConfig):
+    name = 'exable'
---
+++ exable/views.py
@@ -0,0 +1,40 @@
+from .models import Memo
+from django.shortcuts import get_object_or_404
+from .forms import MemoForm
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+def new_memo(request):
+    if request.method == "POST":
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('exable:index')
+    else:   
+        form = MemoForm
+    return render(request, 'exable/new_memo.html', {'form': form })
+
+def index(request):
+  memos = Memo.objects.all().order_by('-updated_datetime')
+  return render(request, 'exable/index.html', { 'memos': memos })
+
+def detail(request, memo_id):
+  memo = get_object_or_404(Memo, id=memo_id)
+  return render(request, 'exable/detail.html', {'memo': memo})
+
+@require_POST
+def delete_memo(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    memo.delete()
+    return redirect('exable:index')
+
+def edit_memo(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    if request.method == "POST":
+        form = MemoForm(request.POST, instance=memo)
+        if form.is_valid():
+            form.save()
+            return redirect('exable:index')
+    else:
+        form = MemoForm(instance=memo)
+    return render(request, 'exable/edit_memo.html', {'form': form, 'memo':memo })
